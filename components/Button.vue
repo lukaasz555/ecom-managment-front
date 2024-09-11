2ed1fc2ee6dc70dfc12c1e3ab1bad3ef1b7316ac
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-09-11 20:57:39 +0200

    chore: add base dependencies, files

---
+++ components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+	<UButton :disabled="disabled">{{ buttonText }}</UButton>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(
+	{
+		type: {
+			type: String,
+			default: 'button',
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		buttonText: {
+			type: String,
+			default: 'Save',
+		},
+	},
+	{
+		emits: ['click'],
+	}
+);
+</script>
---
+++ pages/auth/index.vue
@@ -0,0 +1,4 @@
+<template>
+	<h1>/AUTH</h1>
+	<Button buttonText="Login" />
+</template>
