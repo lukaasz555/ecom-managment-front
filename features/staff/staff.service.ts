a02490d17933ccceabf30580b3b05b605442469d
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-10-04 18:23:46 +0200

    global api + staff service methods

--- features/staff/staff.service.ts
+++ features/staff/staff.service.ts
@@ -1,13 +1,33 @@
 import { ApiService } from '~/services/api.service';
 import type { Staff } from './staff';
 
+const CONTROLLER = 'management';
 export class StaffService extends ApiService {
-	static async getStaffMembers() {
+	static async getStaffMembers(): Promise<Staff[]> {
 		const apiService = new ApiService();
 		const staffMembers = await apiService.getData<Staff[]>(
-			'management',
+			CONTROLLER,
 			'staffMembers'
 		);
 		return staffMembers;
+	}
+
+	static async deleteMember(staffId: number): Promise<void> {
+		const apiService = new ApiService();
+		return await apiService.deleteData(CONTROLLER, `staffMembers/${staffId}`);
+	}
+
+	static async updateMember(staffMember: Staff): Promise<Staff> {
+		const apiService = new ApiService();
+		const updatedMember = await apiService.updateData<Staff>(
+			CONTROLLER,
+			`staffMembers/${staffMember.id}`,
+			staffMember
+		);
+		return updatedMember;
+	}
+
+	static async updatePrivileges(): Promise<void> {
+		// TODO: implement
 	}
 }
--- services/api.service.ts
+++ services/api.service.ts
@@ -36,17 +36,19 @@ export class ApiService {
 		filters?: Filters
 	): Promise<T> {
 		try {
-			const apiUrl = useRuntimeConfig().public.apiUrl;
-			const response: T = await $fetch(`${apiUrl}/${controller}/${method}`, {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${this.#token}`,
-				},
-				params: {
-					...filters,
-				},
-			});
+			const response: T = await $fetch(
+				`${this.#apiUrl}/${controller}/${method}`,
+				{
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${this.#token}`,
+					},
+					params: {
+						...filters,
+					},
+				}
+			);
 			return response;
 		} catch (err) {
 			console.log('getData error', err);
@@ -58,7 +60,63 @@ export class ApiService {
 		}
 	}
 
+	async deleteData<T = void>(
+		controller: string,
+		method: string,
+		body?: Record<string, unknown>
+	): Promise<T> {
+		try {
+			const response: T = await $fetch(
+				`${this.#apiUrl}/${controller}/${method}`,
+				{
+					method: 'DELETE',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${this.#token}`,
+					},
+					body,
+				}
+			);
+			return response;
+		} catch (err) {
+			console.log('deleteData error', err);
+			const errorMessage = new Message('Something went wrong', 'Error');
+			useGlobalStore().addMessage(errorMessage);
+			throw new Error('apiService deleteData error');
+		}
+	}
+
+	async updateData<T, R = T>(
+		controller: string,
+		method: string,
+		body: T
+	): Promise<R> {
+		try {
+			const response: R = await $fetch(
+				`${this.#apiUrl}/${controller}/${method}`,
+				{
+					method: 'PUT',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${this.#token}`,
+					},
+					body,
+				}
+			);
+			return response;
+		} catch (err) {
+			console.log('updateData error', err);
+			const errorMessage = new Message('Something went wrong', 'Error');
+			useGlobalStore().addMessage(errorMessage);
+			throw new Error('apiService updateData error');
+		}
+	}
+
 	get #token() {
 		return useUserStore().token;
+	}
+
+	get #apiUrl() {
+		return useRuntimeConfig().public.apiUrl;
 	}
 }
